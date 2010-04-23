755e9cf00ec814d52f048e57898a2a47d1c5c1b2
antonin07130 <antonin07130@gmail.com>
2010-04-23 15:37:54 -0400

    first stage of QT learning

---
+++ widgets/qtcreator-build/CMakeFiles/widget.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/widget.dir/main.cpp.o"
+  "widget.pdb"
+  "widget"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/widget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
